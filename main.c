b541a5e39beeea837286636e6a363bcd0594cdf8
Robert Hrusecky <robert.hrusecky@utexas.edu>
2019-11-06 20:41:59 -0600

    Able to print simple initrd contents

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Import user configuration: */
 #include <uk/config.h>
+#include <uk/plat/memory.h>
 
-int main(int argc, char *argv[])
+#include <uk/hexdump.h>
+
+int main()
 {
-	printf("Hello world!\n");
+
+    printf("Hello world!\n");
+
+    struct ukplat_memregion_desc desc;
+    ukplat_memregion_get(0, &desc);
+    printf("heap_size: %ld\n", desc.len);
+
+    ukplat_memregion_get(1, &desc);
+    printf("initrd_size: %ld\n", desc.len);
+    uk_hexdumpCd(desc.base, 27);
+    return 0;
 }
