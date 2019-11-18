f8cd3b6c4006b2cf191b96a6cad5cdec2d65b73e
Omar Jamil <omarj@cs.utexas.edu>
2019-11-18 00:48:54 -0600

    made initramfs library and added cpio testcode

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* Import user configuration: */
 #include <uk/config.h>
 #include <uk/plat/memory.h>
-
+#include <uk/initramfs.h>
 #include <uk/hexdump.h>
+#include <uk/alloc.h>
 
 int main()
 {
 
-    printf("Hello world!\n");
+    //printf("Hello world!\n");
 
     struct ukplat_memregion_desc desc;
     ukplat_memregion_get(0, &desc);
@@ -18,6 +19,9 @@ int main()
 
     ukplat_memregion_get(1, &desc);
     printf("initrd_size: %ld\n", desc.len);
-    uk_hexdumpCd(desc.base, desc.len);
+    //uk_hexdumpCd(desc.base, desc.len);
+
+    initramfs_init(&desc);
+    //uk_hexdumpCd(desc.base, 110);
     return 0;
 }
