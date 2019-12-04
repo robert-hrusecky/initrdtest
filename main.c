6f23406f50a987c376a79861ecf9a79ac38110c9
Sachin Beldona <sachin@cs.utexas.edu>
2019-12-03 23:29:09 -0600

    Removed unneseccary test code

--- main.c
+++ main.c
@@ -7,24 +7,11 @@
 /* Import user configuration: */
 #include <uk/config.h>
 #include <uk/plat/memory.h>
-#include <uk/initramfs.h>
 #include <uk/hexdump.h>
 #include <uk/alloc.h>
 #include <dirent.h>
 int main()
 {
-    //printf("Hello world!\n");
-
-    struct ukplat_memregion_desc desc;
-    ukplat_memregion_get(0, &desc);
-    printf("heap_size: %ld\n", desc.len);
-
-    ukplat_memregion_get(1, &desc);
-    printf("initrd_size: %ld\n", desc.len);
-    //uk_hexdumpCd(desc.base, desc.len);
-
-    initramfs_init(&desc);
-    //uk_hexdumpCd(desc.base, 110);
     DIR *dr = opendir("/");
     struct dirent *de;
     while ((de = readdir(dr)) != NULL) {
@@ -35,7 +22,6 @@ int main()
         return -1;
     }
     char buf[1024];
-    //buf[len] = '\0';
     if (read(fd, &buf[0], 100) < 0) {
         return -2;
     }
